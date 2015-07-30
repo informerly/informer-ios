adf0ada8aa7ee10bdf03bbdbbd5f608f55a05301
Apple <you@example.com>
2015-07-30 12:04:39 +0500

    Informally - Day 74
     - Fixes an issue

--- Informerly/Informerly/Utitlities/Utilities.swift
+++ Informerly/Informerly/Utitlities/Utilities.swift
@@ -13,7 +13,6 @@ class Utilities {
         struct Singleton {
             static let instance = Utilities()
         }
-        
         return Singleton.instance
     }
     
@@ -71,24 +70,23 @@ class Utilities {
     
     func isConnectedToNetwork() -> Bool {
         
-//        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
-//        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
-//        zeroAddress.sin_family = sa_family_t(AF_INET)
-//        
-//        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-//            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
-//        }
-//        
-//        var flags: SCNetworkReachabilityFlags = 0
-//        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
-//            return false
-//        }
-//        
-//        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
-//        let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-//        
-//        return (isReachable && !needsConnection) ? true : false
-        return true
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
+        }
+        
+        var flags: SCNetworkReachabilityFlags = 0
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
+            return false
+        }
+        
+        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        return (isReachable && !needsConnection) ? true : false
     }
     
 }
