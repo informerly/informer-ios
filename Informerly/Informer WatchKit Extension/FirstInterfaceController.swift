de97dba43c68384705414761722dc832915d6149
Apple <you@example.com>
2015-09-17 18:49:32 +0500

    Fixes a crash

--- Informerly/Informer WatchKit Extension/FirstInterfaceController.swift
+++ Informerly/Informer WatchKit Extension/FirstInterfaceController.swift
@@ -16,7 +16,7 @@ class FirstInterfaceController: WKInterfaceController {
     @IBOutlet weak var descriptionLabel: WKInterfaceLabel!
     @IBOutlet weak var separator: WKInterfaceSeparator!
     
-    var feeds : [AnyObject]!
+    var feeds : [AnyObject]?
     static var isFirst : Bool = true
     
     override func awakeWithContext(context: AnyObject?) {
@@ -33,7 +33,7 @@ class FirstInterfaceController: WKInterfaceController {
             if Utilities.sharedInstance.getBoolForAppGroupKey(IS_USER_LOGGED_IN) == true {
                 
                 if self.feeds != nil {
-                    self.feeds.removeAll(keepCapacity: false)
+                    self.feeds!.removeAll(keepCapacity: false)
                 } else {
                     self.sourceLabel.setText("")
                     self.titleLabel.setText("")
@@ -49,14 +49,22 @@ class FirstInterfaceController: WKInterfaceController {
     
     func downloadData() {
         WKInterfaceController.openParentApplication(["action":"getFeeds"], reply: { (replyInfo, error) -> Void in
-            self.feeds = replyInfo["result"] as! [AnyObject]
             
-            var interfaceNames = ["FirstController","SecondController","ThirdController","FourthController","FifthController",
-                "SixthController","SeventhController","EightController","NinthController","TenthController"]
-            
-            var data = [self.feeds[0],self.feeds[1],self.feeds[2],self.feeds[3],self.feeds[4],self.feeds[5],self.feeds[6],self.feeds[7],self.feeds[8],self.feeds[9]]
-            
-            WKInterfaceController.reloadRootControllersWithNames(interfaceNames, contexts: data)
+            if (replyInfo != nil) {
+                self.feeds = replyInfo["result"] as? [AnyObject]
+                var interfaceNames = ["FirstController","SecondController","ThirdController","FourthController","FifthController",
+                    "SixthController","SeventhController","EightController","NinthController","TenthController"]
+                
+                var data = [self.feeds![0],self.feeds![1],self.feeds![2],self.feeds![3],self.feeds![4],self.feeds![5],self.feeds![6],self.feeds![7],self.feeds![8],self.feeds![9]]
+                
+                WKInterfaceController.reloadRootControllersWithNames(interfaceNames, contexts: data)
+                
+            } else {
+                self.sourceLabel.setText("")
+                self.titleLabel.setText("")
+                self.separator.setHidden(true)
+                self.descriptionLabel.setText("Unable to load data. Please try again ...")
+            }
         })
     }
     
